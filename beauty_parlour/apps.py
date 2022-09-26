098f4e50a9e87f9f268eaab3d38e5cfe3e2f7e11
Sinon <malovu01@mail.ru>
2022-09-26 16:28:32 +0600

    first commit

---
+++ beauty_parlour/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BeautyParlourConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "beauty_parlour"
---
+++ beauty_parlour/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Master(models.Model):
+    pass
+
+
+class Services(models.Model):
+    pass
+
+
+class Request(models.Model):
+    pass
