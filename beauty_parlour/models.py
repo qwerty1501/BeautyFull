7e1b2b0b62bbfecf25e05b9a6345091d34b229be
Ridzen <ridze00@gmail.com>
2022-09-26 06:16:31 -0500

    done models almost

--- beauty_parlour/models.py
+++ beauty_parlour/models.py
@@ -2,12 +2,48 @@ from django.db import models
 
 
 class Master(models.Model):
-    pass
+
+    """ Модель мастеров """
+
+    name = models.CharField(
+        max_length=250, null=True, blank=False, verbose_name='Имя'
+    )
+    picture = models.ImageField(
+        null=True, blank=True, verbose_name='Картинка для профиля'
+    )
+    information = models.TextField(
+        verbose_name='Информация', null=True, blank=True
+    )
+    experience = models.TextField(
+        verbose_name='Опыт', null=True, blank=True
+    )
+    position = models.CharField(
+        max_length=250, verbose_name='Позиция', null=True, blank=True
+    )
+
+    class Meta:
+        db_table = 'masters'
+        verbose_name = 'Мастер'
+        verbose_name_plural = 'Мастера'
 
 
 class Services(models.Model):
-    pass
+
+    title = models.CharField(
+        max_length=255, verbose_name='Название услуг'
+    )
+    master = models.ForeignKey(
+        to=Master, on_delete=models.CASCADE, verbose_name='Мастер'
+    )
+
+    class Meta:
+        db_table = 'services'
+        verbose_name = 'Услуга'
+        verbose_name_plural = "Услуги"
 
 
 class Request(models.Model):
-    pass
+
+    master = models.ForeignKey(
+
+    )
